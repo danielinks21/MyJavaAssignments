74b74ce8dc60a1ee256c73b10e92d1d0157d7e89
danielinks21 <Dannyung26@gmail.com>
2025-05-09 21:22:18 +0100

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ATM {
+    private Account userAccount;
+    private Scanner scanner;
+
+    public ATM(Account account) {
+        this.userAccount = account;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("\nWelcome to the ATM!");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit Money");
+            System.out.println("3. Withdraw Money");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    userAccount.checkBalance();
+                    break;
+                case 2:
+                    System.out.print("Enter deposit amount: ");
+                    double depositAmount = scanner.nextDouble();
+                    userAccount.deposit(depositAmount);
+                    break;
+                case 3:
+                    System.out.print("Enter withdrawal amount: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    userAccount.withdraw(withdrawAmount);
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+        }
+    }
+}
